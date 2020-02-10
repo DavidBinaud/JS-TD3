3084a90c0c5b2b1596c33236cebeec3f721305a8
David Binaud <david.binaud@etu.umontpellier.fr>
2020-02-10 11:40:21 +0100

    Fin exo 1

--- src/js/element.js
+++ src/js/element.js
@@ -1,19 +1,25 @@
 class Element {
 
 	constructor(x,y,id) {
-		// à compléter
+		this.coordX = x;
+		this.coordY = y;
+		this.sprite = document.getElementById(id);
 	}
 
 	initialiser(x,y,str) {
-		// à compléter
+		this.coordX = x;
+		this.coordY = y;
+		setSrc(str);
+		placer();
 	}
 
 	setSrc(str) {
-		// à compléter
+		this.sprite = str;
 	}
 
 	placer() {
-		// à compléter
+		this.sprite.style.top = "" + (51 + 20 * (this.coordY-1)) + "px"
+		this.sprite.style.left = "" + (51 + 20 * (this.coordX-1)) + "px";
 	}
 
 }
